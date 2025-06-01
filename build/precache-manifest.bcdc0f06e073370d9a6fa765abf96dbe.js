22f9a346541351badb70ea22c98753a427b92a9f
AMAIDI MOHAMED <amaidim30@gmail.com>
2025-06-01 23:57:07 +0000

    Setup GitHub Pages deploy

---
+++ build/precache-manifest.bcdc0f06e073370d9a6fa765abf96dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b001d9d51aa20d943c9963df72b7aa55",
+    "url": "/coffee_ui/index.html"
+  },
+  {
+    "revision": "dcfccb58dd7efcc7a324",
+    "url": "/coffee_ui/static/css/main.f19af16e.chunk.css"
+  },
+  {
+    "revision": "6593f894d016e85388cb",
+    "url": "/coffee_ui/static/js/2.c7821bdd.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/coffee_ui/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "dcfccb58dd7efcc7a324",
+    "url": "/coffee_ui/static/js/main.81c60884.chunk.js"
+  },
+  {
+    "revision": "09c83159e5a3bde42650",
+    "url": "/coffee_ui/static/js/runtime~main.57229eed.js"
+  }
+]);
